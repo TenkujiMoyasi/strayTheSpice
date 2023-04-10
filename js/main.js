31ca35da749ab589669ec05d659fe9dc83e1c9d5
nakayama <nakayama@techno-create.net>
2023-04-10 18:11:52 +0900

    230410 ターン経過、ドロー、捨てるなどの処理を実験 実験用ボタンの実装

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 "use strict"
 {
+  const deckAmount = document.getElementById("deckAmount");
+  const handAmount = document.getElementById("handAmount");
+  const trashAmount = document.getElementById("trashAmount");
+  const turnCounter = document.getElementById("turnCounter");
+  const drawButton = document.getElementById("drawButton");
+  const trashButton = document.getElementById("trashButton");
+  const endButton = document.getElementById("turnEndButton");
+  const startButton = document.getElementById("turnStartButton");
+
+  let turnCount = 0;
+  let turnPlayer = 1;
+
   // const firstDeck = [1, 1, 1, 1, 1, 2, 2, 2, 3, 3,];
   const firstDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   let mainDeck = [...firstDeck];
@@ -26,22 +38,76 @@
 
   // デッキトップからカードをドローする
   function drawCards(n) {
-    if (n < mainDeck.length) {
+    if (n <= mainDeck.length) {
       hands.splice(hands.length, 0, ...(mainDeck.splice(0, n)))
-    } else {
+    }
+    else
+    if(0 === mainDeck.length) {
+      mainDeck.splice(mainDeck.length, 0, ...trash.splice(0,trash.length));
+      deckShuffle(mainDeck);
+      hands.splice(hands.length, 0, ...(mainDeck.splice(0, n)));
+    }
+    else {
       //ドロー数がデッキの残りより多い時は引ききってから墓地をデッキに戻してシャッフルする
       let x = n - mainDeck.length;
       hands.splice(hands.length, 0, ...(mainDeck.splice(0, x)));
-      mainDeck.splice(mainDeck.length, 0, ...trash);
+      mainDeck.splice(mainDeck.length, 0, ...trash.splice(0,trash.length));
       deckShuffle(mainDeck);
+      console.log(`x=${x}`);
       hands.splice(hands.length, 0, ...(mainDeck.splice(0, n - x)));
     }
+    countAmount();
   }
 
-  // 手札を左からｎ枚捨てる
+  // 左からn番目の手札を捨てる
   function toTrash(n) {
-    trash.splice(trash.length, 0, ...(trash.splice(0, trash.length - 1,...hands.splice(0,n))));
+    if(hands.length > 0) {
+      trash.splice(trash.length, 0, ...(trash.splice(0, trash.length - 1,...hands.splice(n,1))));
+    }
+    countAmount();
   }
+
+  function countAmount() {
+    turnCounter.textContent = `TURN:${turnCount.toString().padStart(2,0)}`;
+    deckAmount.textContent = `DECK:${mainDeck.length.toString().padStart(2,0)}`;
+    handAmount.textContent = `HANDS:${hands.length.toString().padStart(2,0)}`;
+    trashAmount.textContent = `TRASH:${trash.length.toString().padStart(2,0)}`;
+    console.log(`デッキ内容[${mainDeck}]`);
+    console.log(`手札[${hands}]`);
+    console.log(`墓地[${trash}]`);
+    console.log(`経過ターン[${turnCount}]`);
+  }
+
+  function turnEnd() {
+    if(hands.length > 5) {
+      for(let n = hands.length - 5; n > 0; n--){
+        toTrash(1);
+      }
+    }
+    turnPlayer = 1;
+  }
+  function turnStart() {
+    turnPlayer = 0;
+    turnCount++;
+    drawCards(1);
+  }
+
+  drawButton.addEventListener('click',()=> {
+    drawCards(1);
+  });
+  trashButton.addEventListener('click',()=> {
+    toTrash(0);
+  });
+  endButton.addEventListener('click',()=> {
+    if(turnPlayer === 0){
+      turnEnd();
+    }
+  });
+  startButton.addEventListener('click',()=> {
+    if(turnPlayer === 1){
+      turnStart();
+    }
+  });
 
 
   console.log(`デッキ内容[${mainDeck}]`);
@@ -49,5 +115,7 @@
   console.log(`最終シャッフル後のデッキ内容[${mainDeck}]`);
   drawCards(3);
   console.log(`初手[${hands}]`);
+
+  countAmount();
 
 }
